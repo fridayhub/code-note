6df520e469709808833c5a1bcc9026fbfd231677
hakits <hanglinux@163.com>
2019-11-19 17:09:34 +0800

    update crawl.py

--- python/crawl.py
+++ python/crawl.py
@@ -8,33 +8,28 @@ import urllib.parse
 import os
 import time
 
-base_url = 'http://xxxxxx'
-start_url = 'http://xxxxxxx'
-base_dir = '/home/xxx/dxxxown'
+base_url = 'http://81.110.88.39'
+start_url = 'http://81.110.88.39/L%3A'
+base_dir = '/home/lost+found'
 
-proxies = {
-        "http":"http:127.0.0.1:1080",
-        "https":"http:127.0.0.1:1080",
-        }
 
-def getHtml(url):
+def getHtml(url, i=0):
     print("getHtml", url)
+    time.sleep(1.5)
     try:
         kv = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
-        r = requests.get(url, headers=kv, timeout=30, proxies=proxies)
+        r = requests.get(url, headers=kv, timeout=30)
         r.encoding = 'utf-8'
         return r.text
 
-
-    except requests.URLError as e:
-        print('get html urlerror:{}'.format(e))
-        return ''
-
-    except requests.HTTPError as e:
+    except Exception as e:
         # code / reason / headers 异常
         print('get html httperror:{}'.format(e))
-        return ''
+        if i < 5:
+            getHtml(url, i+1)
+        else:
+            return ''
 
 
 def getDocList(html):
@@ -55,42 +50,51 @@ def createDir(dir):
 
 
 def downFile(url):
-    print('downloading:', url)
-    turl = urllib.parse.unquote(url)
-    path = base_dir + urlparse(turl).path
-    createDir(os.path.split(path)[0])
-    startTime = time.time()
-    with requests.get(url, stream=True,proxies=proxies) as r:
-        contentLength = int(r.headers['content-length'])
-        line = 'content-length: %dB/ %.2fKB/ %.2fMB'
-        line = line % (contentLength, contentLength / 1024, contentLength / 1024 / 1024)
-        print(line)
-        if checkLocalFile(path, contentLength):
-            print("Already download")
-            return
-        downSize = 0
-        with open(path, 'wb') as f:
-            for chunk in r.iter_content(8192):
-                if chunk:
-                    f.write(chunk)
-                downSize += len(chunk)
-                line = '%d KB/s - %.2f MB， 共 %.2f MB'
-                line = line % (
-                downSize / 1024 / (time.time() - startTime), downSize / 1024 / 1024, contentLength / 1024 / 1024)
-                print(line, end='\r')
-                if downSize >= contentLength:
-                    break
-        timeCost = time.time() - startTime
-        line = '共耗时: %.2f s, 平均速度: %.2f KB/s'
-        line = line % (timeCost, downSize / 1024 / timeCost)
-        print(line)
-
+    time.sleep(1)
+    try:
+        print('downloading:', url)
+        turl = urllib.parse.unquote(url)
+        path = base_dir + urlparse(turl).path
+        createDir(os.path.split(path)[0])
+        startTime = time.time()
+        with requests.get(url, stream=True, timeout=5) as r:
+            contentLength = int(r.headers['content-length'])
+            line = 'content-length: %dB/ %.2fKB/ %.2fMB'
+            line = line % (contentLength, contentLength / 1024, contentLength / 1024 / 1024)
+            print(line)
+            if checkLocalFile(path, contentLength):
+                print("Already download")
+                return 
+            downSize = 0
+            with open(path, 'wb') as f:
+                for chunk in r.iter_content(8192):
+                    if chunk:
+                        f.write(chunk)
+                    downSize += len(chunk)
+                    line = '%d KB/s - %.2f MB， 共 %.2f MB'
+                    line = line % (
+                    downSize / 1024 / (time.time() - startTime), downSize / 1024 / 1024, contentLength / 1024 / 1024)
+                    print(line, end='\r')
+                    if downSize >= contentLength:
+                        break
+            timeCost = time.time() - startTime
+            line = '共耗时: %.2f s, 平均速度: %.2f KB/s'
+            line = line % (timeCost, downSize / 1024 / timeCost)
+            print(line)
+    except Exception as e:
+        print(e)
+        time.sleep(1)
 
 def checkLocalFile(filePath, lenght):
-    fileSize = os.path.getsize(filePath)
-    return fileSize == lenght
+    try:
+        print(filePath)
+        fileSize = os.path.getsize(filePath)
+        return fileSize == lenght
+    except Exception as e:
+        return False
 
 def down(urls):
+    notDown = ['.cbr', '.cbz', '.jpg', '.png']
     if urls:
         for url in urls:
             try:
@@ -100,9 +104,13 @@ def down(urls):
                 turls, tfiles = getDocList(html)
                 if turls:
                     down(turls)
-                for file in tfiles:
-                    print('you need to down', base_url + file)
-                    downFile(base_url + file)
+                for dfile in tfiles:
+                   time.sleep(0.01)
+                   if os.path.splitext(dfile)[-1].lower() in notDown or 'Elfquest' in dfile:
+                       print("not down", dfile)
+                       break
+                   print('you need to down', base_url + dfile)
+                   downFile(base_url + dfile)
             except Exception as e:
                 print(e)
     else:
